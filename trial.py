d54254df10313c275a5f16bb2e456ec30e4fa193
Sunny  Kumar <sunny16270@iiitd.ac.in>
2022-07-26 11:13:07 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -228,7 +228,7 @@ else:
         # for mac and linux(here, os.name is 'posix')
         else:
             _ = system('clear')
-    code="SHAM21"
+    code="GREWAL"
     code1="XD"
     code2="62W"
     test="S3"
@@ -280,9 +280,9 @@ else:
             clear()
             print("You have bought hack for 1 day")
             print(f"{purple}---------------Your play time----------------")
-            print("13th July 2022, 05:30 PM - 06:00 PM")
-            print("13th July 2022, 07:30 PM- 08:00 PM")
-            print("13th July 2022, 09:00 PM- 09:30 PM")
+            print("26th July 2022, 05:30 PM - 06:00 PM")
+            print("26th July 2022, 07:30 PM- 08:00 PM")
+            print("26th July 2022, 09:00 PM- 09:30 PM")
             print("--------*ASK YOUR TIME FROM ME*--------")
             print("--------This time will not work--------")
             print("Please play on the given time, and ")
